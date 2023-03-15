d8336752be6905362b69aafcddfc3d5fd1e42e73
tomatoweb-net <marco@tomatoweb.net>
2023-03-15 14:52:11 +0100

    Cambiato il sesign delle pagine di Login Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about' => 'pages#about'
 end
+
