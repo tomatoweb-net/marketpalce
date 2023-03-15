f35722f558dbcc716b196a11619624ff6f1be7db
tomatoweb-net <marco@tomatoweb.net>
2023-03-15 16:52:09 +0100

    creato contenuti

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_104550) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_15_154853) do
+  create_table "contents", force: :cascade do |t|
+    t.string "titolo"
+    t.text "descrizione"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
